989b3ac223835474dff94a52a1e5866377e347d1
wojteq18 <279730@student.pwr.edu.pl>
2025-06-03 14:59:02 +0200

    Wczytywanie obrazu działa

--- Dithering/app/Main.hs
+++ Dithering/app/Main.hs
@@ -1,6 +1,19 @@
-module Main where
-
-import HaskellSay (haskellSay)
+import Codec.Picture (readImage, DynamicImage)
+import System.Environment (getArgs)
+import System.Exit (exitFailure)
 
 main :: IO ()
-main = haskellSay "Hello, Haskell!"
+main = do
+    args <- getArgs
+    case args of
+        [filePath] -> do
+            result <- readImage filePath --readImage :: FilePath -> IO (Either String DynamicImage) - lewa strona to błąd, prawa strona to obraz
+
+            case result of
+                Left err -> do
+                    putStrLn $ "Błąd przy wczytywaniu obrazu: " ++ err
+                    exitFailure
+                Right img -> do
+                    putStrLn "Obraz wczytany poprawnie"
+        _ -> do
+            exitFailure
