9a6b33748eab7384cb94f6b2ebe54d39bd1af75d
wojteq18 <279730@student.pwr.edu.pl>
2025-06-04 22:16:17 +0200

    Piksele przechowywane w wektorze

--- Dithering/app/Main.hs
+++ Dithering/app/Main.hs
@@ -1,7 +1,8 @@
-import Codec.Picture (readImage, DynamicImage)
+import Codec.Picture 
+import Codec.Picture.Types
 import System.Environment (getArgs)
 import System.Exit (exitFailure)
-
+import qualified Data.Vector.Storable as V
 main :: IO ()
 main = do
     args <- getArgs
@@ -14,6 +15,14 @@ main = do
                     putStrLn $ "Błąd przy wczytywaniu obrazu: " ++ err
                     exitFailure
                 Right img -> do
-                    putStrLn "Obraz wczytany poprawnie"
+                    let Image w h dat = convertRGB8 img --dat :: Vector Word8 - dane RGB po kolei: R, G, B, R, G ..., długość 3 * w * h
+                    putStrLn $ "Szerokość: " ++ show w ++ ", Wysokość: " ++ show h
+                    let x = 10
+                    let y = 10
+                    let pixelIndex = (y * w + x) * 3 -- 3 bo RGB
+                    let r = dat V.! pixelIndex
+                    let g = dat V.! (pixelIndex + 1)
+                    let b = dat V.! (pixelIndex + 2)
+                    putStrLn $ "Kolor piksela na pozycji (10, 10): R=" ++ show r ++ ", G=" ++ show g ++ ", B=" ++ show b
         _ -> do
             exitFailure
