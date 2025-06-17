34af3df51723c31d2081232021545efbc60ad128
Tomasz N <troll.yuck@gmail.com>
2025-06-17 14:42:21 +0200

    fix?

--- Dithering/app-gui/Gui.hs
+++ Dithering/app-gui/Gui.hs
@@ -9,28 +9,28 @@ import qualified GI.GLib as GLib
 import Data.GI.Base
 import Control.Monad (void, when)
 import Control.Monad.IO.Class (liftIO)
-import Control.Concurrent (forkIO)
+import Control.Concurrent (forkIO, threadDelay)
 import Control.Concurrent.STM
+import qualified Data.Vector.Storable.Mutable as M
+import Control.Monad.ST (runST)
 import Data.Int (Int32)
 import Data.Maybe (isJust, fromJust)
 import qualified Data.Vector.Storable as V
 import qualified Data.ByteString as BS
 
+import Dithering.Core (DitheringAlgorithm(..), PaletteMetric(..), ImageRGB, applyDithering, vectorToImage, imageToDoubleVector, getImageDimensions, ditherVectorInPlace, atkinsonVectorInPlace, paletteFuncFor)
 import Codec.Picture (readImage, writePng, convertRGB8, imageWidth, imageHeight, imageData)
-import Dithering.Core
 
--- This function is correct.
 imageRGBToPixbuf :: ImageRGB -> IO GdkPixbuf.Pixbuf
 imageRGBToPixbuf img = do
     let vec = imageData img
         byteString = BS.pack $ V.toList vec
     bytes <- GLib.bytesNew (Just byteString)
     GdkPixbuf.pixbufNewFromBytes bytes GdkPixbuf.ColorspaceRgb False 8
-                (fromIntegral $ imageWidth img)
-                (fromIntegral $ imageHeight img)
-                (fromIntegral $ imageWidth img * 3)
+        (fromIntegral $ imageWidth img)
+        (fromIntegral $ imageHeight img)
+        (fromIntegral $ imageWidth img * 3)
 
--- State for the core logic (unchanged).
 data AppState = AppState
     { baseImage      :: Maybe ImageRGB
     , displayedImage :: Maybe ImageRGB
@@ -52,10 +52,8 @@ initialState = AppState
     , fitToWindow    = False
     }
 
--- **THE FIX IS HERE:** A new record to hold all UI widgets and state TVars.
--- This makes passing them around clean and simple.
 data UI = UI
-    { uiWin          :: Gtk.Window
+    { uiWin          :: Gtk.ApplicationWindow
     , uiScrolledWin  :: Gtk.ScrolledWindow
     , uiImgDisplay   :: Gtk.Image
     , uiRunBtn       :: Gtk.Button
@@ -68,46 +66,37 @@ data UI = UI
 
 main :: IO ()
 main = do
-    void $ Gtk.init Nothing
-    
-    -- Create all the widgets and TVars as before.
+    app <- new Gtk.Application [#applicationId := "com.github.tomasz-niedzialek.dithering"]
+    void $ on app #activate (onActivate app)
+    void $ #run app Nothing
+
+onActivate :: Gtk.Application -> IO ()
+onActivate app = do
     appStateTVar <- newTVarIO initialState
     basePixbufTVar <- newTVarIO Nothing
-    win' <- new Gtk.Window [#title := "Haskell Dithering"]; void $ on win' #destroy Gtk.mainQuit; #resize win' 800 600
+    win' <- new Gtk.ApplicationWindow [#application := app, #title := "Haskell Dithering", #defaultWidth := 800, #defaultHeight := 600]
     grid <- new Gtk.Grid [#columnSpacing := 10, #rowSpacing := 10, #margin := 10]
-    scrolledWin' <- new Gtk.ScrolledWindow []; #setHexpand scrolledWin' True; #setVexpand scrolledWin' True; #setShadowType scrolledWin' Gtk.ShadowTypeIn
+    scrolledWin' <- new Gtk.ScrolledWindow []; #setHexpand scrolledWin' True; #setVexpand scrolledWin' True
+    #setShadowType scrolledWin' Gtk.ShadowTypeIn
     imgDisplay' <- new Gtk.Image []; #add scrolledWin' imgDisplay'
     Gtk.gridAttach grid scrolledWin' 0 0 1 10
-    controlsBox <- new Gtk.Box [#orientation := Gtk.OrientationVertical, #spacing := 15]; #setHexpand controlsBox False; Gtk.gridAttach grid controlsBox 1 0 1 1
-    fileBtn <- new Gtk.FileChooserButton [#title := "Choose an Image", #action := Gtk.FileChooserActionOpen]; Gtk.boxPackStart controlsBox fileBtn False False 5
-    algoLabel <- new Gtk.Label [#label := "Algorithm"]; Gtk.boxPackStart controlsBox algoLabel False False 0
-    algoCombo <- new Gtk.ComboBoxText []; Gtk.comboBoxTextAppendText algoCombo "FloydSteinberg"; Gtk.comboBoxTextAppendText algoCombo "Atkinson"; Gtk.comboBoxTextAppendText algoCombo "Ordered"; Gtk.comboBoxTextAppendText algoCombo "SimpleQuantization"; #setActive algoCombo 0; Gtk.boxPackStart controlsBox algoCombo False False 5
-    metricLabel <- new Gtk.Label [#label := "Palette Metric"]; Gtk.boxPackStart controlsBox metricLabel False False 0
-    metricCombo <- new Gtk.ComboBoxText []; Gtk.comboBoxTextAppendText metricCombo "Euclidean"; Gtk.comboBoxTextAppendText metricCombo "Manhattan"; #setActive metricCombo 0; Gtk.boxPackStart controlsBox metricCombo False False 5
-    bitsLabel <- new Gtk.Label [#label := "Bits per channel (1-8)"]; Gtk.boxPackStart controlsBox bitsLabel False False 0
-    bitsScale <- new Gtk.Scale [#orientation := Gtk.OrientationHorizontal, #drawValue := True]; #setRange bitsScale 1 8; #setValue bitsScale 3; #setDigits bitsScale 0; Gtk.boxPackStart controlsBox bitsScale False False 5
-    runBtn' <- new Gtk.Button [#label := "Dither!", #sensitive := False]; Gtk.boxPackStart controlsBox runBtn' False False 10
-    zoomLabel <- new Gtk.Label [#label := "Zoom"]; Gtk.boxPackStart controlsBox zoomLabel False False 0
-    zoomScale' <- new Gtk.Scale [#orientation := Gtk.OrientationHorizontal, #drawValue := True, #sensitive := False]; #setRange zoomScale' 10 200; #setValue zoomScale' 100; #setDigits zoomScale' 0; Gtk.boxPackStart controlsBox zoomScale' False False 5
-    zoomFitCheck' <- new Gtk.CheckButton [#label := "Fit to Window", #sensitive := False]; Gtk.boxPackStart controlsBox zoomFitCheck' False False 5
-    zoomResetBtn' <- new Gtk.Button [#label := "Reset Zoom (100%)", #sensitive := False]; Gtk.boxPackStart controlsBox zoomResetBtn' False False 5
-    saveBtn <- new Gtk.Button [#label := "Save Dithered Image..."]; Gtk.boxPackStart controlsBox saveBtn False False 10
+    controlsBox <- new Gtk.Box [#orientation := Gtk.OrientationVertical, #spacing := 15]; #setHexpand controlsBox False
+    Gtk.gridAttach grid controlsBox 1 0 1 1
+    fileBtn <- new Gtk.FileChooserButton [#title := "Choose an Image", #action := Gtk.FileChooserActionOpen]; #packStart controlsBox fileBtn False False 5
+    algoLabel <- new Gtk.Label [#label := "Algorithm"]; #packStart controlsBox algoLabel False False 0
+    algoCombo <- new Gtk.ComboBoxText []; Gtk.comboBoxTextAppendText algoCombo "FloydSteinberg"; Gtk.comboBoxTextAppendText algoCombo "Atkinson"; Gtk.comboBoxTextAppendText algoCombo "Ordered"; Gtk.comboBoxTextAppendText algoCombo "SimpleQuantization"; #setActive algoCombo 0; #packStart controlsBox algoCombo False False 5
+    metricLabel <- new Gtk.Label [#label := "Palette Metric"]; #packStart controlsBox metricLabel False False 0
+    metricCombo <- new Gtk.ComboBoxText []; Gtk.comboBoxTextAppendText metricCombo "Euclidean"; Gtk.comboBoxTextAppendText metricCombo "Manhattan"; #setActive metricCombo 0; #packStart controlsBox metricCombo False False 5
+    bitsLabel <- new Gtk.Label [#label := "Bits per channel (1-8)"]; #packStart controlsBox bitsLabel False False 0
+    bitsScale <- new Gtk.Scale [#orientation := Gtk.OrientationHorizontal, #drawValue := True]; #setRange bitsScale 1 8; #setValue bitsScale 3; #setDigits bitsScale 0; #packStart controlsBox bitsScale False False 5
+    runBtn' <- new Gtk.Button [#label := "Dither!", #sensitive := False]; #packStart controlsBox runBtn' False False 10
+    zoomLabel <- new Gtk.Label [#label := "Zoom"]; #packStart controlsBox zoomLabel False False 0
+    zoomScale' <- new Gtk.Scale [#orientation := Gtk.OrientationHorizontal, #drawValue := True, #sensitive := False]; #setRange zoomScale' 10 200; #setValue zoomScale' 100; #setDigits zoomScale' 0; #packStart controlsBox zoomScale' False False 5
+    zoomFitCheck' <- new Gtk.CheckButton [#label := "Fit to Window", #sensitive := False]; #packStart controlsBox zoomFitCheck' False False 5
+    zoomResetBtn' <- new Gtk.Button [#label := "Reset Zoom (100%)", #sensitive := False]; #packStart controlsBox zoomResetBtn' False False 5
+    saveBtn <- new Gtk.Button [#label := "Save Dithered Image..."]; #packStart controlsBox saveBtn False False 10
     #add win' grid
-
-    -- Bundle everything into the UI record.
-    let ui = UI
-            { uiWin = win'
-            , uiScrolledWin = scrolledWin'
-            , uiImgDisplay = imgDisplay'
-            , uiRunBtn = runBtn'
-            , uiZoomScale = zoomScale'
-            , uiZoomFitCheck = zoomFitCheck'
-            , uiZoomResetBtn = zoomResetBtn'
-            , uiAppState = appStateTVar
-            , uiBasePixbuf = basePixbufTVar
-            }
-
-    -- Now, set up event handlers by passing the `ui` record.
+    let ui = UI { uiWin = win', uiScrolledWin = scrolledWin', uiImgDisplay = imgDisplay', uiRunBtn = runBtn', uiZoomScale = zoomScale', uiZoomFitCheck = zoomFitCheck', uiZoomResetBtn = zoomResetBtn', uiAppState = appStateTVar, uiBasePixbuf = basePixbufTVar }
     void $ on fileBtn #selectionChanged (#getFilename fileBtn >>= maybe (return ()) (loadAndDisplayOriginalImage ui))
     void $ on (uiRunBtn ui) #clicked (runDithering ui)
     void $ on algoCombo #changed (Gtk.comboBoxTextGetActiveText algoCombo >>= maybe (return ()) (\t -> atomically $ modifyTVar' (uiAppState ui) $ \s -> s { currentAlgo = case t of "Atkinson" -> Atkinson; "Ordered" -> Ordered; "SimpleQuantization" -> SimpleQuantization; _ -> FloydSteinberg } ))
@@ -118,30 +107,18 @@ main = do
     void $ on (uiZoomResetBtn ui) #clicked (do atomically $ modifyTVar' (uiAppState ui) $ \s -> s { zoomLevel = 1.0, fitToWindow = False }; #setValue (uiZoomScale ui) 100; #setActive (uiZoomFitCheck ui) False; updateDisplayedImage ui)
     void $ on (uiScrolledWin ui) #configureEvent (\_ -> do state <- readTVarIO (uiAppState ui); when (fitToWindow state) (updateDisplayedImage ui); return False)
     void $ on saveBtn #clicked (saveDitheredImage ui)
-
     #showAll win'
-    Gtk.main
 
--- **THE FIX IS HERE:** All helper functions now take the `UI` record as an argument,
--- giving them access to all the state and widgets they need.
 updateDisplayedImage :: UI -> IO ()
-updateDisplayedImage ui = void . forkIO $ do
-  state <- readTVarIO (uiAppState ui)
-  readTVarIO (uiBasePixbuf ui) >>= maybe (return ()) (go state)
-  where
-    go state pbuf = do
-      (pWidth, pHeight) <- (,) <$> GdkPixbuf.pixbufGetWidth pbuf <*> GdkPixbuf.pixbufGetHeight pbuf
-      (cWidth, cHeight) <- (,) <$> #getAllocatedWidth (uiScrolledWin ui) <*> #getAllocatedHeight (uiScrolledWin ui)
-      let (newWidth, newHeight) = if fitToWindow state
-            then let scaleX = fromIntegral cWidth / fromIntegral pWidth
-                     scaleY = fromIntegral cHeight / fromIntegral pHeight
-                     scale = min scaleX scaleY
-                 in (floor $ fromIntegral pWidth * scale, floor $ fromIntegral pHeight * scale)
-            else (floor $ fromIntegral pWidth * zoomLevel state, floor $ fromIntegral pHeight * zoomLevel state)
-      mScaled <- GdkPixbuf.pixbufScaleSimple pbuf (fromIntegral newWidth) (fromIntegral newHeight) GdkPixbuf.InterpTypeBilinear
-      void $ GLib.idleAdd GLib.PRIORITY_DEFAULT_IDLE $ do
-        #setFromPixbuf (uiImgDisplay ui) mScaled
-        return False
+updateDisplayedImage ui = void . forkIO $ readTVarIO (uiAppState ui) >>= \state -> readTVarIO (uiBasePixbuf ui) >>= maybe (return ()) (go state)
+  where go state pbuf = do
+          (pWidth, pHeight) <- (,) <$> GdkPixbuf.pixbufGetWidth pbuf <*> GdkPixbuf.pixbufGetHeight pbuf
+          (cWidth, cHeight) <- (,) <$> #getAllocatedWidth (uiScrolledWin ui) <*> #getAllocatedHeight (uiScrolledWin ui)
+          let (newWidth, newHeight) = if fitToWindow state
+                then let scale = min (fromIntegral cWidth / fromIntegral pWidth) (fromIntegral cHeight / fromIntegral pHeight) in (floor $ fromIntegral pWidth * scale, floor $ fromIntegral pHeight * scale)
+                else (floor $ fromIntegral pWidth * zoomLevel state, floor $ fromIntegral pHeight * zoomLevel state)
+          mScaled <- GdkPixbuf.pixbufScaleSimple pbuf (fromIntegral newWidth) (fromIntegral newHeight) GdkPixbuf.InterpTypeBilinear
+          void $ GLib.idleAdd GLib.PRIORITY_DEFAULT_IDLE $ #setFromPixbuf (uiImgDisplay ui) mScaled >> return False
 
 runDithering :: UI -> IO ()
 runDithering ui = do
@@ -149,16 +126,45 @@ runDithering ui = do
   case baseImage state of
     Nothing -> return ()
     Just img -> void . forkIO $ do
-      putStrLn "Dithering process started..."
-      let (w, h) = getImageDimensions img; rawD = imageToDoubleVector img
-      let ditheredD = applyDithering (currentAlgo state) (currentMetric state) (currentNBits state) w h rawD
-      let resultImg = vectorToImage w h ditheredD
-      ditheredPixbuf <- imageRGBToPixbuf resultImg
+      void $ GLib.idleAdd GLib.PRIORITY_DEFAULT_IDLE $ #setSensitive (uiRunBtn ui) False >> return False
+      
+      let (w, h) = getImageDimensions img
+      let rawD = imageToDoubleVector img
+      let paletteFunc = paletteFuncFor (currentMetric state)
+      
+      putStrLn "Stage 1: Quantizing..."
+      let quantD = applyDithering SimpleQuantization (currentMetric state) (currentNBits state) w h rawD
+      let quantImg = vectorToImage w h quantD
+      quantPixbuf <- imageRGBToPixbuf quantImg
+      
       atomically $ do
-        modifyTVar' (uiAppState ui) (\s -> s { displayedImage = Just resultImg })
-        writeTVar (uiBasePixbuf ui) (Just ditheredPixbuf)
-      putStrLn "Dithering process finished."
+        modifyTVar' (uiAppState ui) (\s -> s { displayedImage = Just quantImg })
+        writeTVar (uiBasePixbuf ui) (Just quantPixbuf)
       updateDisplayedImage ui
+      
+      when (currentAlgo state /= SimpleQuantization) $ do
+        threadDelay 1000000
+        putStrLn "Stage 2: Dithering..."
+        let ditheredD = runST $ do
+              mv <- M.new (V.length rawD)
+              case currentAlgo state of
+                FloydSteinberg -> ditherVectorInPlace paletteFunc (currentNBits state) w h rawD mv
+                Atkinson       -> atkinsonVectorInPlace paletteFunc (currentNBits state) w h rawD mv
+                Ordered        -> V.copy mv (applyDithering Ordered (currentMetric state) (currentNBits state) w h rawD)
+                _              -> V.copy mv quantD
+              V.freeze mv
+
+        -- **THE FIX IS HERE:** The let and monadic bindings are on separate lines.
+        let ditheredImg = vectorToImage w h ditheredD
+        ditheredPixbuf <- imageRGBToPixbuf ditheredImg
+        
+        atomically $ do
+          modifyTVar' (uiAppState ui) (\s -> s { displayedImage = Just ditheredImg })
+          writeTVar (uiBasePixbuf ui) (Just ditheredPixbuf)
+        updateDisplayedImage ui
+        
+      putStrLn "All processing finished."
+      void $ GLib.idleAdd GLib.PRIORITY_DEFAULT_IDLE $ #setSensitive (uiRunBtn ui) True >> return False
 
 loadAndDisplayOriginalImage :: UI -> FilePath -> IO ()
 loadAndDisplayOriginalImage ui path = do
@@ -183,7 +189,7 @@ saveDitheredImage ui = do
     when (isJust mImg) $ do
         dialog <- new Gtk.FileChooserDialog [ #title := "Save File", #action := Gtk.FileChooserActionSave ]
         Gtk.dialogAddButton dialog "Cancel" (fromIntegral $ fromEnum Gtk.ResponseTypeCancel)
-        Gtk.dialogAddButton dialog "Save" (fromIntegral $ fromEnum Gtk.ResponseTypeAccept)
+        Gtk.dialogAddButton dialog "Save"   (fromIntegral $ fromEnum Gtk.ResponseTypeAccept)
         #setTransientFor dialog (Just (uiWin ui))
         response <- #run dialog
         when (toEnum (fromIntegral response) == Gtk.ResponseTypeAccept) $ do
--- Dithering/src/Dithering/Core.hs
+++ Dithering/src/Dithering/Core.hs
@@ -3,55 +3,56 @@ module Dithering.Core
     ( -- Types
       DitheringAlgorithm(..)
     , PaletteMetric(..)
-    , ImageRGB -- Re-exporting for convenience
+    , ImageRGB
       -- Core Functions
     , applyDithering
     , vectorToImage
     , imageToDoubleVector
     , getImageDimensions
+    , paletteFuncFor
+    -- Exporting the memory-efficient in-place functions for the GUI
+    , ditherVectorInPlace
+    , atkinsonVectorInPlace
     ) where
 
--- Removed 'convertRGB8' from the import list as it was unused.
 import Codec.Picture (Image(Image), PixelRGB8, imageWidth, imageHeight, imageData)
 import qualified Data.Vector.Storable as V
 import Control.Monad (forM_, when)
-import Control.Monad.ST (runST)
+import Control.Monad.ST (runST, ST)
 import qualified Data.Vector.Storable.Mutable as M
 import Data.List (minimumBy)
 import Data.Word (Word8)
 
--- Types are unchanged...
 type ImageRGB = Image PixelRGB8
 type Pixel = (Double, Double, Double)
 type PaletteFunc = Int -> Pixel -> Pixel
 data DitheringAlgorithm = FloydSteinberg | Atkinson | Ordered | SimpleQuantization deriving (Show, Eq)
 data PaletteMetric = Euclidean | Manhattan deriving (Show, Eq)
 
--- Top-level functions are unchanged...
 applyDithering :: DitheringAlgorithm -> PaletteMetric -> Int -> Int -> Int -> V.Vector Double -> V.Vector Double
 applyDithering SimpleQuantization _ nBits _ _ vec = quantizeVector nBits vec
 applyDithering FloydSteinberg metric nBits width height vec = ditherVector (paletteFuncFor metric) nBits width height vec
 applyDithering Atkinson metric nBits width height vec = atkinsonVector (paletteFuncFor metric) nBits width height vec
 applyDithering Ordered _ nBits width height vec = orderedDitherVector bayerMatrix4x4 nBits width height vec
+
 getImageDimensions :: ImageRGB -> (Int, Int)
 getImageDimensions img = (imageWidth img, imageHeight img)
 imageToDoubleVector :: ImageRGB -> V.Vector Double
 imageToDoubleVector = V.map fromIntegral . imageData
 vectorToImage :: Int -> Int -> V.Vector Double -> ImageRGB
-vectorToImage width height vecD =
-    let vecW8 = V.map (\x -> fromIntegral (round (clamp x) :: Integer) :: Word8) vecD
-    in Image width height vecW8
+vectorToImage width height vecD = let vecW8 = V.map (\x -> fromIntegral (round (clamp x) :: Integer) :: Word8) vecD in Image width height vecW8
 
--- Helper and algorithm implementations...
 paletteFuncFor :: PaletteMetric -> PaletteFunc
 paletteFuncFor Euclidean = paletteFuncEuklides
 paletteFuncFor Manhattan = paletteFuncManhattan
+
 getIndex :: Int -> Int -> Int -> Int -> Int
 getIndex width x y c = (y * width + x) * 3 + c
 clamp :: Double -> Double
 clamp x | x < 0 = 0 | x > 255 = 255 | otherwise = x
 bayerMatrix4x4 :: [[Int]]
 bayerMatrix4x4 = [ [ 0,  8,  2, 10], [12,  4, 14,  6], [ 3, 11,  1,  9], [15,  7, 13,  5] ]
+
 quantizeChannel :: Int -> Double -> Double 
 quantizeChannel n value =
     let levels = (2^n) :: Int
@@ -59,14 +60,12 @@ quantizeChannel n value =
         idx = round (value / step) :: Int
     in clamp (fromIntegral idx * step)
 
--- **Fix for the type ambiguity warning is here**
 quantizeChannelOrdered :: Int -> Double -> Int -> Int -> Double
 quantizeChannelOrdered nBits originalValue bayerVal matrixMaxValPlusOne =
     let numLevels = (2^nBits) :: Int
         step = if numLevels <= 1 then 255.0 else 255.0 / fromIntegral (numLevels - 1)
         thresholdAdjustment = ( (fromIntegral bayerVal / fromIntegral matrixMaxValPlusOne) - 0.5) * step
         modifiedValue = originalValue + thresholdAdjustment
-        -- By adding `:: Int`, we tell the compiler exactly which type `levelIndex` should be.
         levelIndex = if numLevels <= 1 then 0 else round (clamp modifiedValue / step) :: Int
         quantizedValue = fromIntegral levelIndex * step
     in clamp quantizedValue
@@ -76,14 +75,13 @@ quantizeVector nBits = V.map (quantizeChannel nBits)
 generatePalette :: Int -> [Pixel]
 generatePalette n = let levels = [0, 255 / ((2^n) - 1) .. 255] in [(r,g,b) | r <- levels, g <- levels, b <- levels]
 findClosestManhattan :: Pixel -> [Pixel] -> Pixel
-findClosestManhattan (r,g,b) = minimumBy (\(r1,g1,b1) (r2,g2,b2) -> compare (dist (r1,g1,b1)) (dist (r2,g2,b2)))
-  where dist (r',g',b') = abs (r - r') + abs (g - g') + abs (b - b')
+findClosestManhattan (r,g,b) = minimumBy (\(r1,g1,b1) (r2,g2,b2) -> compare (dist (r1,g1,b1)) (dist (r2,g2,b2))) where dist (r',g',b') = abs (r - r') + abs (g - g') + abs (b - b')
 paletteFuncManhattan :: PaletteFunc
 paletteFuncManhattan n px = findClosestManhattan px (generatePalette n)
 paletteFuncEuklides :: PaletteFunc
 paletteFuncEuklides n (r,g,b) = (quantizeChannel n r, quantizeChannel n g, quantizeChannel n b)
 
--- All other functions are unchanged...
+-- **THE FIX IS HERE:** The `orderedDitherVector` function is now correctly formatted.
 orderedDitherVector :: [[Int]] -> Int -> Int -> Int -> V.Vector Double -> V.Vector Double
 orderedDitherVector bayerMatrix nBits width height vec = runST $ do
     mv <- V.thaw vec
@@ -91,58 +89,72 @@ orderedDitherVector bayerMatrix nBits width height vec = runST $ do
     let matrixW = if matrixH > 0 then length (head bayerMatrix) else 0
     let bayerMatrixMaxValPlusOne = matrixH * matrixW
     when (matrixH == 0 || matrixW == 0) $ error "Bayer matrix cannot be empty"
+
     forM_ [0 .. height - 1] $ \y ->
       forM_ [0 .. width  - 1] $ \x -> do
         let bayerVal = (bayerMatrix !! (y `mod` matrixH)) !! (x `mod` matrixW)
+
+        -- Red Channel
         let idxR = getIndex width x y 0
         oldR <- M.read mv idxR
         M.write mv idxR (quantizeChannelOrdered nBits oldR bayerVal bayerMatrixMaxValPlusOne)
+
+        -- Green Channel
         let idxG = getIndex width x y 1
         oldG <- M.read mv idxG
         M.write mv idxG (quantizeChannelOrdered nBits oldG bayerVal bayerMatrixMaxValPlusOne)
+
+        -- Blue Channel
         let idxB = getIndex width x y 2
         oldB <- M.read mv idxB
         M.write mv idxB (quantizeChannelOrdered nBits oldB bayerVal bayerMatrixMaxValPlusOne)
+
     V.freeze mv
-ditherVector :: PaletteFunc -> Int -> Int -> Int -> V.Vector Double -> V.Vector Double
-ditherVector paletteFunc nBits width height vec = runST $ do
-    mv <- V.thaw vec
-    forM_ [0 .. height - 1] $ \y ->
-      forM_ [0 .. width  - 1] $ \x -> do
-        let idxR = getIndex width x y 0
-            idxG = getIndex width x y 1
-            idxB = getIndex width x y 2
-        oldR <- M.read mv idxR
-        oldG <- M.read mv idxG
-        oldB <- M.read mv idxB
-        let (newR, newG, newB) = paletteFunc nBits (oldR, oldG, oldB)
-        M.write mv idxR newR
-        M.write mv idxG newG
-        M.write mv idxB newB
-        let errR = oldR - newR; errG = oldG - newG; errB = oldB - newB
-            prop dx dy w = do
-              let x' = x + dx; y' = y + dy
-              when (x' >= 0 && x' < width && y' >= 0 && y' < height) $ do
-                let iR = getIndex width x' y' 0; iG = getIndex width x' y' 1; iB = getIndex width x' y' 2
-                vR <- M.read mv iR; vG <- M.read mv iG; vB <- M.read mv iB
-                M.write mv iR (vR + errR * w); M.write mv iG (vG + errG * w); M.write mv iB (vB + errB * w)
-        prop 1 0 (7/16); prop (-1) 1 (3/16); prop 0 1 (5/16); prop 1 1 (1/16)
-    V.freeze mv
-atkinsonVector :: PaletteFunc -> Int -> Int -> Int -> V.Vector Double -> V.Vector Double
-atkinsonVector paletteFunc nBits width height vec = runST $ do
-    mv <- V.thaw vec
+
+ditherVectorInPlace :: PaletteFunc -> Int -> Int -> Int -> V.Vector Double -> M.MVector s Double -> ST s ()
+ditherVectorInPlace paletteFunc nBits width height originalVec targetVec = do
+    V.copy targetVec originalVec
     forM_ [0 .. height - 1] $ \y ->
       forM_ [0 .. width  - 1] $ \x -> do
         let idxR = getIndex width x y 0; idxG = getIndex width x y 1; idxB = getIndex width x y 2
-        oldR <- M.read mv idxR; oldG <- M.read mv idxG; oldB <- M.read mv idxB
+        oldR <- M.read targetVec idxR; oldG <- M.read targetVec idxG; oldB <- M.read targetVec idxB
         let (newR, newG, newB) = paletteFunc nBits (oldR, oldG, oldB)
-        M.write mv idxR newR; M.write mv idxG newG; M.write mv idxB newB
+        M.write targetVec idxR newR; M.write targetVec idxG newG; M.write targetVec idxB newB
         let errR = oldR - newR; errG = oldG - newG; errB = oldB - newB
             prop dx dy w = do
               let x' = x + dx; y' = y + dy
               when (x' >= 0 && x' < width && y' >= 0 && y' < height) $ do
                 let iR = getIndex width x' y' 0; iG = getIndex width x' y' 1; iB = getIndex width x' y' 2
-                vR <- M.read mv iR; vG <- M.read mv iG; vB <- M.read mv iB
-                M.write mv iR (vR + errR * w); M.write mv iG (vG + errG * w); M.write mv iB (vB + errB * w)
+                vR <- M.read targetVec iR; vG <- M.read targetVec iG; vB <- M.read targetVec iB
+                M.write targetVec iR (vR + errR * w); M.write targetVec iG (vG + errG * w); M.write targetVec iB (vB + errB * w)
+        prop 1 0 (7/16); prop (-1) 1 (3/16); prop 0 1 (5/16); prop 1 1 (1/16)
+
+ditherVector :: PaletteFunc -> Int -> Int -> Int -> V.Vector Double -> V.Vector Double
+ditherVector paletteFunc nBits width height vec = runST $ do
+    mv <- M.new (V.length vec)
+    ditherVectorInPlace paletteFunc nBits width height vec mv
+    V.freeze mv
+
+atkinsonVectorInPlace :: PaletteFunc -> Int -> Int -> Int -> V.Vector Double -> M.MVector s Double -> ST s ()
+atkinsonVectorInPlace paletteFunc nBits width height originalVec targetVec = do
+    V.copy targetVec originalVec
+    forM_ [0 .. height - 1] $ \y ->
+      forM_ [0 .. width  - 1] $ \x -> do
+        let idxR = getIndex width x y 0; idxG = getIndex width x y 1; idxB = getIndex width x y 2
+        oldR <- M.read targetVec idxR; oldG <- M.read targetVec idxG; oldB <- M.read targetVec idxB
+        let (newR, newG, newB) = paletteFunc nBits (oldR, oldG, oldB)
+        M.write targetVec idxR newR; M.write targetVec idxG newG; M.write targetVec idxB newB
+        let errR = oldR - newR; errG = oldG - newG; errB = oldB - newB
+            prop dx dy w = do
+              let x' = x + dx; y' = y + dy
+              when (x' >= 0 && x' < width && y' >= 0 && y' < height) $ do
+                let iR = getIndex width x' y' 0; iG = getIndex width x' y' 1; iB = getIndex width x' y' 2
+                vR <- M.read targetVec iR; vG <- M.read targetVec iG; vB <- M.read targetVec iB
+                M.write targetVec iR (vR + errR * w); M.write targetVec iG (vG + errG * w); M.write targetVec iB (vB + errB * w)
         prop 1 0 (1/8); prop (-1) 1 (1/8); prop 0 1 (1/8); prop 1 1 (1/8); prop 2 0 (1/8); prop 0 2 (1/8)
+
+atkinsonVector :: PaletteFunc -> Int -> Int -> Int -> V.Vector Double -> V.Vector Double
+atkinsonVector paletteFunc nBits width height vec = runST $ do
+    mv <- M.new (V.length vec)
+    atkinsonVectorInPlace paletteFunc nBits width height vec mv
     V.freeze mv
